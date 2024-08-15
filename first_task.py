f572e3ed1917d36f7a27a73ffd6dd237d7294804
FDevGoIt <n.lyman@goit.ua>
2024-08-15 23:47:01 +0300

    add tasks

--- first_task.py
+++ first_task.py
@@ -1,2 +1,16 @@
+from datetime import datetime
+
 def get_days_from_today(date):
-    return date
+    try:
+        now = datetime.now()
+        normalized_string = date.replace(date[4],".")
+        normalized_date = datetime.strptime(normalized_string, "%Y.%m.%d") 
+        days_differense =  normalized_date-now
+        return days_differense.days
+
+    except ValueError:
+        return "Please enter the correct date format, for example 2020-02-12"
+
+    
+    
+print(get_days_from_today("202-10-09"))
