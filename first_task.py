3bc6c45146100639b76476fc66502a1cb05977c5
FDevGoIt <n.lyman@goit.ua>
2024-11-30 15:32:17 +0200

    fix

--- first_task.py
+++ first_task.py
@@ -5,12 +5,12 @@ def get_days_from_today(date):
         now = datetime.now()
         normalized_string = date.replace(date[4],".")
         normalized_date = datetime.strptime(normalized_string, "%Y.%m.%d") 
-        days_differense =  normalized_date-now
-        return days_differense.days  + 1
+        days_differense =  normalized_date.date()-now.date()
+        return days_differense.days 
 
     except ValueError:
         return "Please enter the correct date format, for example 2020-02-12"
 
     
     
-print(get_days_from_today("2024-11-29"))
+print(get_days_from_today("2024-12-01"))
--- second_task.py
+++ second_task.py
@@ -3,21 +3,10 @@ import random
 def get_numbers_ticket(min, max, quantity):
     if max - min < quantity - 1:
          return []
-    
-    ticket_set  = set()
-
     if min < 1 or max > 1000:
         return "min cannot be less than 1, max cannot be more than 1000"
-    for i in range(quantity):  
-            current_number = random.randint(min, max)
-            ticket_set.add(current_number) 
-            if len(ticket_set) < quantity:
-                 for i in range(quantity):  
-                    current_number = random.randint(min, max)
-                    ticket_set.add(current_number)
-    return sorted(random.sample(list(ticket_set), k=quantity))
+    return sorted(random.sample(list(range(min,max+1)), k=quantity))
 
 
-
-lottery_numbers = get_numbers_ticket(10, 14, 6)
+lottery_numbers = get_numbers_ticket(10, 14, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
